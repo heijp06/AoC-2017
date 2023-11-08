31cd481aa3b44c17877979d88db25c7f5d44c6c4
Peter van der Heijden <heijp06@gmail.com>
2023-11-08 18:08:15 +0100

    Simplify code.

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -6,13 +6,13 @@ module Lib
 import Data.Char(digitToInt)
 
 part1 :: String -> Int
-part1 xs = sum $ [ a | (a, b) <- zip digits (tail digits), a == b ]
-    where
-        digits = [ digitToInt c | c <- xs ++ [head xs] ]
+part1 = calculateSum 1
 
 part2 :: String -> Int
-part2 xs = sum $ [ a | (a, b) <- zip digits cycled, a == b ]
-    where
-        digits = [ digitToInt c | c <- xs ]
-        n = length xs `div` 2
-        cycled = drop n digits ++ take n digits
+part2 xs = calculateSum (length xs `div` 2) xs
+
+calculateSum :: Int -> String -> Int
+calculateSum n xs = sum $ [ digitToInt a | (a, b) <- zip xs (rotate n xs), a == b ]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
