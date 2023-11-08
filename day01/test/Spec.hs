6000d5d3686456a91ad8e70d16906c32ec220d12
Peter van der Heijden <heijp06@gmail.com>
2023-11-08 19:30:14 +0100

    Refactor tests.

--- day01/test/Spec.hs
+++ day01/test/Spec.hs
@@ -4,26 +4,32 @@ import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 import Lib (part1, part2)
 
-examples :: [(String -> Int, String, Int)]
-examples = [ (part1, "1122", 3)
-           , (part1, "1111", 4)
-           , (part1, "1234", 0)
-           , (part1, "91212129", 9)
-           , (part2, "1212", 6)
-           , (part2, "1221", 0)
-           , (part2, "123425", 4)
-           , (part2, "123123", 12)
-           , (part2, "12131415", 4)
-           ]
+examples1 :: [(String, Int)]
+examples1 = [ ("1122", 3)
+            , ("1111", 4)
+            , ("1234", 0)
+            , ("91212129", 9)
+            ]
+
+
+examples2 :: [(String, Int)]
+examples2 = [ ("1212", 6)
+            , ("1221", 0)
+            , ("123425", 4)
+            , ("123123", 12)
+            , ("12131415", 4)
+            ]
 
 main :: IO ()
 main = hspecWith defaultConfig {configFailFast = True} specs
 
 specs :: Spec
-specs = describe "captcha -> sum" $ for_ examples test
-  where
-    test (part, captcha, sum') = it description assertion
-      where
-        description = captcha ++ " -> " ++ show sum'
-        assertion = result `shouldBe` sum'
-        result = part captcha
+specs = do
+   describe "part1: captcha -> sum" $ for_ examples1 (test part1)
+   describe "part2: captcha -> sum" $ for_ examples2 (test part2)
+    where
+      test part (captcha, sum') = it description assertion
+        where
+          description = captcha ++ " -> " ++ show sum'
+          assertion = result `shouldBe` sum'
+          result = part captcha
