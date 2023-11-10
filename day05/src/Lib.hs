db96a233c8e4a975f96e4e65cc74969fdfa20104
Peter van der Heijden <heijp06@gmail.com>
2023-11-10 21:18:41 +0100

    Improve speed by using a Map.

--- day05/src/Lib.hs
+++ day05/src/Lib.hs
@@ -6,8 +6,9 @@ module Lib
     ) where
 
 import Control.Monad.State.Lazy (State, evalState, get, put)
+import Data.Map (Map, (!), adjust, fromList)
 
-data Cpu = Cpu { code :: [Int]
+data Cpu = Cpu { code :: Map Int Int
                , index :: Int
                , steps :: Int
                }
@@ -21,7 +22,7 @@ part2 :: [Int] -> Int
 part2 = solve $ \x -> if x >= 3 then x - 1 else x + 1
 
 solve :: Updater -> [Int] -> Int
-solve updater xs = evalState (run updater) Cpu { code = xs
+solve updater xs = evalState (run updater) Cpu { code = fromList $ zip [0..] xs
                                                , index = 0
                                                , steps = 0
                                                }
@@ -30,11 +31,11 @@ run :: Updater -> State Cpu Int
 run updater = do
     Cpu{..} <- get
     let newSteps = steps + 1
-    let offset = code !! index
+    let offset = code ! index
     let newIndex = index + offset
     if newIndex < 0 || newIndex >= length code
         then return newSteps
         else do
-            let newCode = take index code ++ [updater offset] ++ drop (index + 1) code
+            let newCode = adjust updater index code
             put Cpu { code = newCode, index = newIndex, steps = newSteps }
             run updater
