8e1938e29ceaefbbf3dc2c6fe20a417d5deb7954
Peter van der Heijden <heijp06@gmail.com>
2023-11-11 20:31:54 +0100

    Solve day 6 part 1.

--- day06/src/Lib.hs
+++ day06/src/Lib.hs
@@ -1,10 +1,28 @@
 module Lib
-    ( part1
+    ( next
+    , part1
     , part2
     ) where
 
+import Data.List (maximumBy)
+
+import Data.Set (Set, empty, insert, member)
+
 part1 :: [Int] -> Int
-part1 = undefined
+part1 xs = go xs empty 0
+    where
+        go ys seen n | member ys seen = n
+        go ys seen n = go (next ys) (insert ys seen) (n + 1)
 
 part2 :: [Int] -> Int
 part2 = undefined
+
+next :: [Int] -> [Int]
+next xs = zipWith (+) without toAdd
+    where
+        (number, index) = maximumBy (\(n1, i1) (n2, i2) -> compare (n1, -i1) (n2, -i2)) $ zip xs [0..]
+        len = length xs
+        (d, m) = divMod number $ len
+        toAdd = rotate (len - index - 1) $ replicate m (d + 1) ++ replicate (len - m) d
+        rotate n ys = drop n ys ++ take n ys
+        without = take index xs ++ [0] ++ drop (index + 1) xs
--- day06/test/Spec.hs
+++ day06/test/Spec.hs
@@ -1,5 +1,5 @@
 import Test.Hspec (describe, hspec, it, shouldBe)
-import Lib (part1, part2)
+import Lib (next, part1, part2)
 
 example :: [Int]
 example = [ 0, 2, 7, 0 ]
@@ -7,4 +7,6 @@ example = [ 0, 2, 7, 0 ]
 main :: IO ()
 main = hspec $ do
     describe "Part 1" $ do
+        it "next example" $ next example `shouldBe` [ 2, 4, 1, 2]
+
         it "part1 example" $ part1 example `shouldBe` 5
