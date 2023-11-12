d4ca39d8a5b075b22d73caa563007adc8d401a85
Peter van der Heijden <heijp06@gmail.com>
2023-11-12 11:41:52 +0100

    Solve day 7 part 1.

--- day07/src/Lib.hs
+++ day07/src/Lib.hs
@@ -4,7 +4,13 @@ module Lib
     ) where
 
 part1 :: [String] -> String
-part1 = undefined
+part1 xs = head [ x | x <- bottoms (map words xs), x `notElem` tops (map words xs) ]
 
 part2 :: [String] -> String
 part2 = undefined
+
+bottoms :: [[String]] -> [String]
+bottoms = map head
+
+tops :: [[String]] -> [String]
+tops = map (filter (/=',')) . concatMap (drop 3)
