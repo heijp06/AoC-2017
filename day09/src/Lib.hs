3ece33ac55db346687194da2ce001e38cd3a9e16
Peter van der Heijden <heijp06@gmail.com>
2023-11-12 20:24:09 +0100

    Start implementing day 9 part 1, first test passes.

--- day09/src/Lib.hs
+++ day09/src/Lib.hs
@@ -3,8 +3,13 @@ module Lib
     , part2
     ) where
 
+type State = (Int, String)
+
 part1 :: String -> Int
-part1 = undefined
+part1 xs = fst . group 1 $ tail xs
 
 part2 :: String -> Int
 part2 = undefined
+
+group :: Int -> String -> State
+group level xs = (level, tail xs)
