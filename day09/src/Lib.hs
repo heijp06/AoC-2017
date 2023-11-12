6a3ea95e6fba8011cfba0abea0ee724323c402ce
Peter van der Heijden <heijp06@gmail.com>
2023-11-12 20:34:51 +0100

    2 tests pass.

--- day09/src/Lib.hs
+++ day09/src/Lib.hs
@@ -12,4 +12,7 @@ part2 :: String -> Int
 part2 = undefined
 
 group :: Int -> String -> State
-group level xs = (level, tail xs)
+group level ('}':xs) = (level, xs)
+group level ('{':xs) = let (result, ys) = group (level + 1) xs in (level + result, ys)
+group level [] = error $ "Unexpected end of string in group, level = " ++ show level
+group level xs = error $ "Unexpected characters '" ++ xs ++ "' in group, level = " ++ show level
