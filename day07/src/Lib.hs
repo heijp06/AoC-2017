b643a9d481fcb6f1ea21109767b75832a515d57d
Peter van der Heijden <heijp06@gmail.com>
2023-11-12 13:47:46 +0100

    Solve day 7 part 1.

--- day07/src/Lib.hs
+++ day07/src/Lib.hs
@@ -3,6 +3,9 @@ module Lib
     , part2
     ) where
 
+import Data.Function (on)
+import Data.List (group, maximumBy, nub, sort)
+import Data.List.Extra (allSame)
 import Data.Map ((!))
 import qualified Data.Map as M
 
@@ -12,7 +15,26 @@ part1 :: [String] -> String
 part1 xs = head [ x | x <- bottoms (map words xs), x `notElem` tops (map words xs) ]
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 xs = balance $ children tree
+    where
+        tree = buildTree (buildNodes xs) (part1 xs)
+
+balance :: [Tree] -> Int
+balance [t1, t2] = if isBalanced t1 then balance (children t2) else balance (children t1)
+balance ts = if allSame (map weight ts') then expectedWeight - sum (map weight ts') else balance ts'
+    where
+        expectedWeight = head . maximumBy (compare `on` length) . group . sort $ map weight ts
+        unbalanced = head $ filter (\t -> weight t /= expectedWeight) ts
+        ts' = children unbalanced
+
+children :: Tree -> [Tree]
+children (Node _ _ ts) = ts
+
+weight :: Tree -> Int
+weight (Node _ w ts) = w + sum [ weight t | t <- ts ]
+
+isBalanced :: Tree -> Bool
+isBalanced (Node _ _ ts) = length (nub [ weight t | t <- ts ]) == 1
 
 bottoms :: [[String]] -> [String]
 bottoms = map head
