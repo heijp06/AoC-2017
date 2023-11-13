ea49034cf3e55e6dea8affa3656afa5efbf98cd1
Peter van der Heijden <heijp06@gmail.com>
2023-11-13 21:46:39 +0100

    Solve day 9 part 2.

--- day09/src/Lib.hs
+++ day09/src/Lib.hs
@@ -6,26 +6,30 @@ module Lib
 import Text.ParserCombinators.ReadP
 
 part1 :: String -> Int
-part1 = fst . head . readP_to_S (groupParser 1)
+part1 = fst . parse
 
-groupParser :: Int -> ReadP Int
-groupParser level = do
+part2 :: String -> Int
+part2 = snd . parse
+
+parse :: String -> (Int, Int)
+parse = fst . head . readP_to_S (groupParser (1, 0))
+
+groupParser :: (Int, Int) -> ReadP (Int, Int)
+groupParser (level, canceled) = do
     _ <- char '{'
-    xs <- sepBy (groupParser (level + 1) +++ garbageParser) (char ',')
+    xs <- sepBy (groupParser (level + 1, 0) +++ garbageParser) (char ',')
     _ <- char '}'
-    return $ sum xs + level
+    return $ (sum (map fst xs) + level, sum (map snd xs) + canceled)
 
-garbageParser :: ReadP Int
+garbageParser :: ReadP (Int, Int)
 garbageParser = do
     _ <- char '<'
-    _ <- many (ignoreParser <++ satisfy (/='>'))
+    canceled <- length . filter (/='>') <$> many (ignoreParser <++ satisfy (/='>'))
     _ <- char '>'
-    return 0
+    return (0, canceled)
 
 ignoreParser :: ReadP Char
 ignoreParser = do
     _ <- char '!'
-    get
-
-part2 :: String -> Int
-part2 = undefined
+    _ <- get
+    return '>'
