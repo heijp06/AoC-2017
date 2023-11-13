4ca7391d88eb48046f85eb28e7ae54d6b309aea6
Peter van der Heijden <heijp06@gmail.com>
2023-11-13 21:27:43 +0100

    Solve day 9 part 1.

--- day09/src/Lib.hs
+++ day09/src/Lib.hs
@@ -1,28 +1,31 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Lib
     ( part1
     , part2
     ) where
 
-data State = State { level :: Int
-                   , total :: Int
-                   , stream :: String
-                   }
+import Text.ParserCombinators.ReadP
 
 part1 :: String -> Int
-part1 xs = total $ many State { level = 1
-                               , total = 0
-                               , stream = xs
-                               }
+part1 = fst . head . readP_to_S (groupParser 1)
+
+groupParser :: Int -> ReadP Int
+groupParser level = do
+    _ <- char '{'
+    xs <- sepBy (groupParser (level + 1) +++ garbageParser) (char ',')
+    _ <- char '}'
+    return $ sum xs + level
+
+garbageParser :: ReadP Int
+garbageParser = do
+    _ <- char '<'
+    _ <- many (ignoreParser <++ satisfy (/='>'))
+    _ <- char '>'
+    return 0
+
+ignoreParser :: ReadP Char
+ignoreParser = do
+    _ <- char '!'
+    get
 
 part2 :: String -> Int
 part2 = undefined
-
-many :: State -> State
-many state@State{..} | head stream == '{' = group state
-many state = state
-
-group :: State -> State
-group state@State{..} | head stream == '}' = state
-group state@State{..} = group state { level = level + 1, total = total + level, stream = tail stream }
