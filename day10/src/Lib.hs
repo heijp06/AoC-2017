afa9cb184be8837667b76b4f3516a3c2289087aa
Peter van der Heijden <heijp06@gmail.com>
2023-11-14 19:57:33 +0100

    Solve day 10 part 1.

--- day10/src/Lib.hs
+++ day10/src/Lib.hs
@@ -1,16 +1,24 @@
 module Lib
     ( part1
     , part2
+    , reverseAt
     , reverseFirst
     , rotateLeft
     , rotateRight
+    , solve
     ) where
 
 part1 :: [Int] -> Int
-part1 = undefined
+part1 = solve 256
 
 part2 :: [Int] -> Int
 part2 = undefined
+
+solve :: Int -> [Int] -> Int
+solve n xs = head result * (head . tail) result
+    where
+        (result, _, _) = foldl tie ([0..n-1], 0, 0) xs
+        tie (ys, pos, skip) x = (reverseAt pos x ys, (pos + x + skip) `mod` n, skip + 1)
 
 rotateLeft :: Int -> [a] -> [a]
 rotateLeft n xs = let n' = n `mod` length xs in drop n' xs ++ take n' xs
@@ -20,3 +28,6 @@ rotateRight = rotateLeft . negate
 
 reverseFirst :: Int -> [a] -> [a]
 reverseFirst n xs = (reverse . take n) xs ++ drop n xs
+
+reverseAt :: Int -> Int -> [a] -> [a]
+reverseAt index len = rotateRight index . reverseFirst len . rotateLeft index
--- day10/test/Spec.hs
+++ day10/test/Spec.hs
@@ -1,6 +1,9 @@
-import Test.Hspec
+import Test.Hspec hiding (example)
 
 import Lib
+
+example :: [Int]
+example = [3, 4, 1, 5]
 
 main :: IO ()
 main = hspec $ do
@@ -12,3 +15,9 @@ main = hspec $ do
 
     describe "reverseFirst" $ do
         it "reverseFirst 2" $ reverseFirst 2 "abcde" `shouldBe` "bacde"
+    
+    describe "reverseAt" $ do
+        it "reverseAt 2 2" $ reverseAt 2 2 "abcde" `shouldBe` "abdce"
+    
+    describe "solve" $ do
+        it "solve example" $ solve 5 example `shouldBe` 12
