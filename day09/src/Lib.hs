8493dd80efc57ea7ba6290a2228c13e77ee8518d
Peter van der Heijden <peter.vd.heijden@malvernpanalytical.com>
2023-11-14 10:26:28 +0100

    Use sepBy as infix function.

--- day09/src/Lib.hs
+++ day09/src/Lib.hs
@@ -17,7 +17,7 @@ parse = fst . head . readP_to_S (group (1, 0))
 group :: (Int, Int) -> ReadP (Int, Int)
 group (score, nonCanceled) = do
     _ <- char '{'
-    xs <- sepBy (group (score + 1, 0) +++ garbage) (char ',')
+    xs <- (group (score + 1, 0) +++ garbage) `sepBy` (char ',')
     _ <- char '}'
     return (sum (map fst xs) + score, sum (map snd xs) + nonCanceled)
 
