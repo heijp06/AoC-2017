b61e434238dbafe7e473aad9021d6bc410979daa
Peter van der Heijden <heijp06@gmail.com>
2023-11-15 20:12:48 +0100

    Day 11 part 1, tests succeed, fails on input.

--- day11/src/Lib.hs
+++ day11/src/Lib.hs
@@ -3,8 +3,26 @@ module Lib
     , part2
     ) where
 
+type Position = (Int, Int)
+
 part1 :: [String] -> Int
-part1 = undefined
+part1 xs = a + b
+    where
+        (x, y) = foldl add (0, 0) $ map dirToPos xs
+        a = abs x `div` 2
+        b = abs (abs y - a) `div` 2
 
 part2 :: [String] -> Int
 part2 = undefined
+
+dirToPos :: String -> Position
+dirToPos "n" = (0, 2)
+dirToPos "ne" = (2, 1)
+dirToPos "se" = (2, -1)
+dirToPos "s" = (0, -2)
+dirToPos "sw" = (-2, -1)
+dirToPos "nw" = (-2, 1)
+dirToPos xs = error $ "Unknown direction: " ++ xs
+
+add :: Position -> Position -> Position
+add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
