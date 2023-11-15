d8a37a13e034b02bcee444a1c36315bf9d6bd1de
Peter van der Heijden <heijp06@gmail.com>
2023-11-15 21:13:13 +0100

    Add code for part 2, currently failing.

--- day11/src/Lib.hs
+++ day11/src/Lib.hs
@@ -6,14 +6,22 @@ module Lib
 type Position = (Int, Int)
 
 part1 :: [String] -> Int
-part1 xs = a + b
-    where
-        (x, y) = foldr (add . dirToPos) (0, 0) xs
-        a = abs x `div` 2
-        b = max 0 $ (abs y - a) `div` 2
+part1 = fst . solve
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 = snd . solve
+
+solve :: [String] -> (Int, Int)
+solve xs = (distance end, furthest)
+    where
+        (end, furthest) = foldr (add . dirToPos) ((0, 0), 0) xs
+        add (x1, y1) ((x2, y2), d) = let pos = (x1 + x2, y1 + y2) in (pos, max d $ distance pos)
+
+distance :: Position -> Int
+distance (x, y) = a + b
+    where
+        a = abs x `div` 2
+        b = max 0 $ (abs y - a) `div` 2
 
 dirToPos :: String -> Position
 dirToPos "n" = (0, 2)
@@ -23,6 +31,3 @@ dirToPos "s" = (0, -2)
 dirToPos "sw" = (-2, -1)
 dirToPos "nw" = (-2, 1)
 dirToPos xs = error $ "Unknown direction: " ++ xs
-
-add :: Position -> Position -> Position
-add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
