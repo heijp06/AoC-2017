25e1f698b09728067b4a8c669b3909330d834863
Peter van der Heijden <heijp06@gmail.com>
2023-11-16 19:40:36 +0100

    Start day 12.

---
+++ day12/src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib
+    ( part1
+    , part2
+    ) where
+
+part1 :: [String] -> Int
+part1 = undefined 
+
+part2 :: [String] -> Int
+part2 = undefined 
---
+++ day12/test/Spec.hs
@@ -0,0 +1,18 @@
+import Test.Hspec hiding (example)
+
+import Lib
+
+example :: [String]
+example = [ "0 <-> 2"
+          , "1 <-> 1"
+          , "2 <-> 0, 3, 4"
+          , "3 <-> 2, 4"
+          , "4 <-> 2, 3, 6"
+          , "5 <-> 6"
+          , "6 <-> 4, 5"
+          ]
+
+main :: IO ()
+main = hspec $ do
+    describe "Part 1" $ do
+        it "part1 example == 6" $ part1 example `shouldBe` 6
