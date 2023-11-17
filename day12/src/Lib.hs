eccf84618c897276aeb468f1db1acb720b5c5886
Peter van der Heijden <heijp06@gmail.com>
2023-11-17 06:55:00 +0100

    Solve day 12 part 1.

--- day12/src/Lib.hs
+++ day12/src/Lib.hs
@@ -3,8 +3,26 @@ module Lib
     , part2
     ) where
 
+import Data.List.Split (splitOn)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Map ((!))
+
 part1 :: [String] -> Int
-part1 = undefined 
+part1 xs = solve Set.empty (Set.singleton 0) (parse xs)
 
 part2 :: [String] -> Int
 part2 = undefined 
+
+parse :: [String] -> Map.Map Int [Int]
+parse = foldr addRow Map.empty
+    where
+        addRow x m = let [prog, progs] = splitOn " <-> " x in Map.insert (read prog) (map read (splitOn "," progs)) m
+
+solve :: Set.Set Int -> Set.Set Int -> Map.Map Int [Int] -> Int
+solve reachable current pipes
+    | Set.null current = Set.size reachable
+    | otherwise = solve newReachable newCurrent pipes
+        where
+            newReachable = Set.union reachable current
+            newCurrent = Set.fromList [ to | from <- Set.toList current, to <- pipes ! from, not $ Set.member to reachable ]
