a11e94a920816e43faa97e99a55a90d66c872236
Peter van der Heijden <heijp06@gmail.com>
2023-11-17 20:36:05 +0100

    Solve day 12 part 2.

--- day12/src/Lib.hs
+++ day12/src/Lib.hs
@@ -16,7 +16,7 @@ part1 :: [String] -> Int
 part1 xs = Set.size $ solve Set.empty (Set.singleton 0) (parse xs)
 
 part2 :: [String] -> Int
-part2 = undefined 
+part2 xs = solve2 [] (parse xs)
 
 parse :: [String] -> Pipes
 parse = foldr addRow Map.empty
@@ -33,3 +33,9 @@ solve reachable current pipes
         where
             newReachable = Set.union reachable current
             newCurrent = Set.fromList [ to | from <- Set.toList current, to <- pipes ! from, Set.notMember to reachable ]
+
+solve2 :: [Programs] -> Pipes -> Int
+solve2 programs pipes = go [k | k <- Map.keys pipes, all (Set.notMember k) programs ]
+    where
+        go [] = length programs
+        go (x:_) = solve2 (solve Set.empty (Set.singleton x) pipes:programs) pipes
--- day12/test/Spec.hs
+++ day12/test/Spec.hs
@@ -16,3 +16,6 @@ main :: IO ()
 main = hspec $ do
     describe "Part 1" $ do
         it "part1 example == 6" $ part1 example `shouldBe` 6
+
+    describe "Part 2" $ do
+        it "part2 example == 2" $ part2 example `shouldBe` 2
