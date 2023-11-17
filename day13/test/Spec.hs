312bb01fd7ec0b673c57ca7f38062ed9d229d4cf
Peter van der Heijden <heijp06@gmail.com>
2023-11-17 22:06:33 +0100

    Add tests.

--- day13/test/Spec.hs
+++ day13/test/Spec.hs
@@ -1,2 +1,16 @@
+import Test.Hspec hiding (example)
+
+import Lib
+
+example :: [String]
+example = [ "0: 3"
+          , "1: 2"
+          , "4: 4"
+          , "6: 4"
+          ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Part 1" $ do
+        it "part1 example == 24" $ part1 example `shouldBe` 24
+
