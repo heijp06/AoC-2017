947ccd8af39100caabf8c992740bcb14bafa604b
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 08:44:22 +0100

    Refactor.

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -6,7 +6,7 @@ module Lib
 import Data.List.Split (splitOn)
 
 part1 :: [String] -> Int
-part1 xs = sum [ d * r | (d, r) <- map parseLine xs, d `mod` (2 * r - 2) == 0 ]
+part1 xs = sum [ d * r | (d, r) <- map parseLine xs, caught r d ]
 
 part2 :: [String] -> Int
 part2 xs = solve 0 [ layer d r | (d, r) <- map parseLine xs ]
@@ -16,8 +16,11 @@ parseLine line = case splitOn ": " line of
                     [depth, range] -> (read depth, read range)
                     _ -> error $ "Unknown line: " ++ line
 
+caught :: Int -> (Int -> Bool)
+caught r = (==0) . (`mod` (2 * r - 2))
+
 layer :: Int -> Int -> [Bool]
-layer d r = let m = 2 * r - 2 in cycle [ (x + d) `mod` m == 0 | x <- [0..m-1] ]
+layer d r = cycle [ caught r (x + d) | x <- [0..2*r-3] ]
 
 solve :: Int -> [[Bool]] -> Int
 solve n sieve | not (any head sieve) = n
