a1226c0ad3a8d6ad9552625c1a0470abd5367919
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 08:09:54 +0100

    Refactor.

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -11,9 +11,7 @@ part1 :: [String] -> Int
 part1 xs = sum [ d * r | (d, r) <- map parseLine xs, d `mod` (2 * r - 2) == 0 ]
 
 part2 :: [String] -> Int
-part2 xs = solve 0 sieve
-    where
-        sieve = [ layer d r | (d, r) <- map parseLine xs ]
+part2 xs = solve 0 [ layer d r | (d, r) <- map parseLine xs ]
 
 parseLine :: String -> (Int, Int)
 parseLine line = case splitOn ": " line of
@@ -21,9 +19,7 @@ parseLine line = case splitOn ": " line of
                     _ -> error $ "Unknown line: " ++ line
 
 layer :: Int -> Int -> [Bool]
-layer d r = cycle [ (x + d) `mod` m == 0 | x <- [0..m-1] ]
-    where
-        m = 2 * r - 2
+layer d r = let m = 2 * r - 2 in cycle [ (x + d) `mod` m == 0 | x <- [0..m-1] ]
 
 solve :: Int -> Sieve -> Int
 solve n sieve
