9e675cb6e97899804833303dbe1455ee9219b532
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 10:40:39 +0100

    Start day 14 part 2.

--- day14/src/Lib.hs
+++ day14/src/Lib.hs
@@ -3,31 +3,42 @@ module Lib
     , part2
     ) where
 
+import qualified Data.Set as Set
 import Knot (hash)
 
+type Square = (Int, Int)
+type Region = Set.Set Square
+
 part1 :: String -> Int
-part1 xs = sum $ concatMap (map count) hashes
+part1 xs = length . filter (=='1') $ concatMap (concatMap toBin) hashes
     where
         hashes = [ hash $ xs ++ "-" ++ show x | x <- [0..127] :: [Int] ]
 
 part2 :: String -> Int
-part2 = undefined
+part2 xs = error $ show (length used)
+    where
+        hashes = [ hash $ xs ++ "-" ++ show x | x <- [0..127] :: [Int] ]
+        grid = concatMap (concatMap toBin) hashes
+        squares = zip ((,) <$> [0..127] <*> [0..127]) grid
+        used = [ s | (s, bit) <- squares, bit == '1' ]
 
-count :: Char -> Int
-count '0' = 0
-count '1' = 1
-count '2' = 1
-count '3' = 2
-count '4' = 1
-count '5' = 2
-count '6' = 2
-count '7' = 3
-count '8' = 1
-count '9' = 2
-count 'a' = 2
-count 'b' = 3
-count 'c' = 2
-count 'd' = 3
-count 'e' = 3
-count 'f' = 4
-count x = error $ "Illegal character " ++ show x
+asd = undefined
+
+toBin :: Char -> String
+toBin '0' = "0000"
+toBin '1' = "0001"
+toBin '2' = "0010"
+toBin '3' = "0011"
+toBin '4' = "0100"
+toBin '5' = "0101"
+toBin '6' = "0110"
+toBin '7' = "0111"
+toBin '8' = "1000"
+toBin '9' = "1001"
+toBin 'a' = "1010"
+toBin 'b' = "1011"
+toBin 'c' = "1100"
+toBin 'd' = "1101"
+toBin 'e' = "1110"
+toBin 'f' = "1111"
+toBin x = error $ "Illegal character " ++ show x
--- day14/test/Spec.hs
+++ day14/test/Spec.hs
@@ -8,3 +8,6 @@ main :: IO ()
 main = hspec $ do
     describe "Part 1" $ do
         it "part1 example == 8108" $ part1 example `shouldBe` 8108
+
+    describe "Part 2" $ do
+        it "part2 example == 1242" $ part2 example `shouldBe` 1242
