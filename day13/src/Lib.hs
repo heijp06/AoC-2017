aa83a4fa32e1a16f443666c368267356f3a08d93
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 08:05:23 +0100

    Solve day 13 part 2.

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -5,13 +5,27 @@ module Lib
 
 import Data.List.Split (splitOn)
 
+type Sieve = [[Bool]]
+
 part1 :: [String] -> Int
 part1 xs = sum [ d * r | (d, r) <- map parseLine xs, d `mod` (2 * r - 2) == 0 ]
 
 part2 :: [String] -> Int
-part2 = undefined
+part2 xs = solve 0 sieve
+    where
+        sieve = [ layer d r | (d, r) <- map parseLine xs ]
 
 parseLine :: String -> (Int, Int)
 parseLine line = case splitOn ": " line of
                     [depth, range] -> (read depth, read range)
                     _ -> error $ "Unknown line: " ++ line
+
+layer :: Int -> Int -> [Bool]
+layer d r = cycle [ (x + d) `mod` m == 0 | x <- [0..m-1] ]
+    where
+        m = 2 * r - 2
+
+solve :: Int -> Sieve -> Int
+solve n sieve
+    | not (any head sieve) = n
+solve n sieve = solve (n + 1) (map tail sieve)
--- day13/test/Spec.hs
+++ day13/test/Spec.hs
@@ -14,3 +14,5 @@ main = hspec $ do
     describe "Part 1" $ do
         it "part1 example == 24" $ part1 example `shouldBe` 24
 
+    describe "Part 2" $ do
+        it "part2 example == 10" $ part2 example `shouldBe` 10
