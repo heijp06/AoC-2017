ffd510bcd297f7bf023a2558ebf6ab8d9819d9ca
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 09:58:59 +0100

    Solve day 14 part 1.

--- day14/src/Lib.hs
+++ day14/src/Lib.hs
@@ -6,7 +6,28 @@ module Lib
 import Knot (hash)
 
 part1 :: String -> Int
-part1 = undefined
+part1 xs = sum $ concatMap (map count) hashes
+    where
+        hashes = [ hash $ xs ++ "-" ++ show x | x <- [0..127] :: [Int] ]
 
 part2 :: String -> Int
 part2 = undefined
+
+count :: Char -> Int
+count '0' = 0
+count '1' = 1
+count '2' = 1
+count '3' = 2
+count '4' = 1
+count '5' = 2
+count '6' = 2
+count '7' = 3
+count '8' = 1
+count '9' = 2
+count 'a' = 2
+count 'b' = 3
+count 'c' = 2
+count 'd' = 3
+count 'e' = 3
+count 'f' = 4
+count x = error $ "Illegal character " ++ show x
