e5897287c316b6d9495d182b209aa34aa5d05cdf
Peter van der Heijden <heijp06@gmail.com>
2023-11-18 23:15:34 +0100

    Solve day 15 part 2.

--- day15/src/Lib.hs
+++ day15/src/Lib.hs
@@ -11,4 +11,8 @@ part1 a b = sum [ 1 | (u, v) <- zip generatorA generatorB, (u `mod` 65536) == (v
         generator start factor = take 40000000 . drop 1 $ iterate ((`mod` 2147483647) . (*factor)) start
 
 part2 :: Int -> Int -> Int
-part2 = undefined
+part2 a b = sum [ 1 | (u, v) <- zip generatorA generatorB, (u `mod` 65536) == (v `mod` 65536) ]
+    where
+        generatorA = take 5000000 . filter ((==0) . (`mod` 4)) $ generator a 16807
+        generatorB = take 5000000 . filter ((==0) . (`mod` 8)) $ generator b 48271
+        generator start factor = drop 1 $ iterate ((`mod` 2147483647) . (*factor)) start
--- day15/test/Spec.hs
+++ day15/test/Spec.hs
@@ -6,3 +6,5 @@ main = hspec $ do
     describe "Part 1" $ do
         it "part1 65 8921" $ part1 65 8921 `shouldBe` 588
 
+    describe "Part 2" $ do
+        it "part2 65 8921" $ part2 65 8921 `shouldBe` 309
