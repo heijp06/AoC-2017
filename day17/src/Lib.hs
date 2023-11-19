b402f60900ab324ba50175ae2d2e3cb223ec900f
Peter van der Heijden <heijp06@gmail.com>
2023-11-19 12:15:55 +0100

    Solve day 17 part 1.

--- day17/src/Lib.hs
+++ day17/src/Lib.hs
@@ -4,7 +4,15 @@ module Lib
     ) where
 
 part1 :: Int -> Int
-part1 = undefined
+part1 step = (xs ++ xs) !! (position + 1)
+    where
+        (xs, position) = foldl (add step) ([0], 0) [1..2017]
 
 part2 :: Int -> Int
 part2 = undefined
+
+add :: Int -> ([Int], Int) -> Int -> ([Int], Int)
+add step (xs, position) x = (prefix ++ [x] ++ suffix, newPosition + 1)
+    where
+        newPosition = (position + step) `mod` length xs
+        (prefix, suffix) = splitAt (newPosition + 1) xs
