64495b88e6719fd53e7bbcea442f40609414e478
Peter van der Heijden <heijp06@gmail.com>
2023-11-19 19:55:42 +0100

    Implementing commands.

--- day18/src/Lib.hs
+++ day18/src/Lib.hs
@@ -5,37 +5,116 @@ module Lib
     , part2
     ) where
 
-import Control.Monad.State.Lazy (State, evalState, get, put)
-import Data.Char (isDigit)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.State.Lazy (StateT, evalStateT, get, put)
+import Data.Char (isLower)
 import qualified Data.Map as Map
 import Data.List.Split (splitOn)
 
 data Cpu = Cpu { code :: [String]
                , programCounter :: Int
                , registers :: Map.Map Char Int
+               , sound :: Maybe Int
+               , recovered :: Maybe Int
                }
 
-type Program = State Cpu
+type Program = StateT Cpu (Either String)
 
 part1 :: [String] -> Int
-part1 xs = evalState firstRecover Cpu { code = xs
-                                      , programCounter = 0
-                                      , registers = Map.empty
-                                      }
+part1 xs = case evalStateT firstRecover (newCpu xs) of
+            Right value -> value
+            Left message -> error message
 
 part2 :: [String] -> Int
 part2 = undefined
 
+newCpu :: [String] -> Cpu
+newCpu xs = Cpu { code = xs
+                , programCounter = 0
+                , registers = Map.empty
+                , sound = Nothing
+                , recovered = Nothing
+                }
+
 firstRecover :: Program Int
 firstRecover = do
+    command <- getCommand
+    case splitOn " " command of
+        ["set", register, value] -> set register value
+        ["add", register, value] -> add register value
+        ["mul", register, value] -> mul register value
+        ["mod", register, value] -> mod' register value
+        ["snd", value] -> snd' value
+        ["rcv", register] -> rcv register
+        ["jgz", register, value] -> jgz register value
+        _ -> raise $ "Unknown command: " ++ command
     Cpu{..} <- get
-    let command = code !! programCounter
-    case splitOn command " " of
-        ["set", register, number] | (isDigit . head) number -> setNumber (head register) $ read number
-        _ -> error "Unknown command"
-    return 0
+    case recovered of
+        Just x -> return x
+        _ -> firstRecover
 
-setNumber :: Char -> Int -> Program ()
-setNumber register value = do
+getCommand :: Program String
+getCommand = do
     cpu@Cpu{..} <- get
-    put cpu { registers = Map.insert register value registers }
+    put cpu { programCounter = programCounter + 1 }
+    return $ code !! programCounter
+
+getValue :: String -> Program Int
+getValue register | (isLower . head) register = do
+    Cpu{..} <- get
+    let r = head register
+    return $ Map.findWithDefault 0 r registers
+getValue number = return $ read number
+
+set :: String -> String -> Program ()
+set register value = do
+    cpu@Cpu{..} <- get
+    x <- getValue value
+    let r = head register
+    put cpu { registers = Map.insert r x registers }
+
+add :: String -> String -> Program ()
+add = calcNumber (+)
+
+mul :: String -> String -> Program ()
+mul = calcNumber (*)
+
+mod' :: String -> String -> Program ()
+mod' = calcNumber (flip mod)
+
+calcNumber :: (Int -> Int -> Int) -> String -> String -> Program ()
+calcNumber op register value = do
+    Cpu{..} <- get
+    x <- getValue value
+    let r = head register
+    set register $ show(x `op` Map.findWithDefault 0 r registers)
+
+snd' :: String -> Program ()
+snd' value = do
+    x <- getValue value
+    cpu <- get
+    put cpu { sound = Just x }
+
+rcv :: String -> Program ()
+rcv register = do
+    value <- getValue register
+    if value == 0
+        then
+            return ()
+        else do
+            cpu@Cpu{..} <- get
+            put cpu { recovered = sound }
+
+jgz :: String -> String -> Program ()
+jgz register value = do
+    r <- getValue register
+    if r == 0
+        then
+            return ()
+        else do
+            v <- getValue value
+            cpu@Cpu{..} <- get
+            put cpu { programCounter = programCounter + v - 1 }
+
+raise :: String -> Program ()
+raise message = lift $ Left message
