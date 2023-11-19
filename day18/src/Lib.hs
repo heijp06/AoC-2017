f7aae6615b0ae719687a1c90d1914334749f5101
Peter van der Heijden <heijp06@gmail.com>
2023-11-19 16:28:59 +0100

    Start day 18 part 1.

--- day18/src/Lib.hs
+++ day18/src/Lib.hs
@@ -1,10 +1,41 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
     ( part1
     , part2
     ) where
 
+import Control.Monad.State.Lazy (State, evalState, get, put)
+import Data.Char (isDigit)
+import qualified Data.Map as Map
+import Data.List.Split (splitOn)
+
+data Cpu = Cpu { code :: [String]
+               , programCounter :: Int
+               , registers :: Map.Map Char Int
+               }
+
+type Program = State Cpu
+
 part1 :: [String] -> Int
-part1 = undefined
+part1 xs = evalState firstRecover Cpu { code = xs
+                                      , programCounter = 0
+                                      , registers = Map.empty
+                                      }
 
 part2 :: [String] -> Int
 part2 = undefined
+
+firstRecover :: Program Int
+firstRecover = do
+    Cpu{..} <- get
+    let command = code !! programCounter
+    case splitOn command " " of
+        ["set", register, number] | (isDigit . head) number -> setNumber (head register) $ read number
+        _ -> error "Unknown command"
+    return 0
+
+setNumber :: Char -> Int -> Program ()
+setNumber register value = do
+    cpu@Cpu{..} <- get
+    put cpu { registers = Map.insert register value registers }
