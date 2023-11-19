b1e77f56bb5f4a0a53256652e259d704697d3b5f
Peter van der Heijden <heijp06@gmail.com>
2023-11-19 15:20:40 +0100

    Solve day 17 part 2.

--- day17/src/Lib.hs
+++ day17/src/Lib.hs
@@ -9,10 +9,16 @@ part1 step = (xs ++ xs) !! (position + 1)
         (xs, position) = foldl (add1 step) ([0], 0) [1..2017]
 
 part2 :: Int -> Int
-part2 = undefined
+part2 step = fst $ foldl (add2 step) (0, 0) [1..50000000]
 
 add1 :: Int -> ([Int], Int) -> Int -> ([Int], Int)
 add1 step (xs, position) x = (prefix ++ [x] ++ suffix, newPosition)
     where
-        newPosition = 1 + (position + step) `mod` length xs
+        newPosition = 1 + (position + step) `mod` x
         (prefix, suffix) = splitAt newPosition xs
+
+add2 :: Int -> (Int, Int) -> Int -> (Int, Int)
+add2 step (value, position) x = (newValue, newPosition)
+    where
+        newPosition = 1 + (position + step) `mod` x
+        newValue = if newPosition == 1 then x else value
