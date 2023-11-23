2ecc471f782dba2ec7e15943710d8a165bf74ed9
Peter van der Heijden <heijp06@gmail.com>
2023-11-23 18:33:28 +0100

    Solve day 20 part 1.

--- day20/src/Lib.hs
+++ day20/src/Lib.hs
@@ -3,10 +3,14 @@ module Lib
     , part2
     ) where
 
+import Data.Function (on)
+import Data.List (minimumBy)
 import Particle
 
 part1 :: [String] -> Int
-part1 = undefined
+part1 = fst . minimumBy (compare `on` (magnitude . acceleration . snd)) . zip [0..] . map parse
+    where
+        magnitude (x, y, z) = x * x + y * y + z * z
 
 part2 :: [String] -> Int
 part2 = undefined
