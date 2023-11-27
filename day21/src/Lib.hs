3cb9c21ea531bf4a3858ff449eec040885c669c2
Peter van der Heijden <heijp06@gmail.com>
2023-11-27 20:44:03 +0100

    Solve day 21 part2.

--- day21/src/Lib.hs
+++ day21/src/Lib.hs
@@ -9,14 +9,11 @@ import Data.Map ((!))
 import qualified Data.Map as Map
 import Rules
 
-import Debug.Trace (trace)
-import Text.Printf (printf)
-
 start :: String
 start = ".#...####"
 
 part1 :: [String] -> Int
-part1 xs = length $ filter (/='.') step5
+part1 xs = length $ filter (=='#') step5
     where
         rules = parse xs
         step1 = enhance rules start -- 3 --> 4
@@ -26,29 +23,29 @@ part1 xs = length $ filter (/='.') step5
         step5 = enhance rules step4 -- 12 --> 18
 
 part2 :: [String] -> Int
-part2  xs = undefined
+part2 xs = sum $ map calculate (Map.toList result)
     where
         rules = parse xs
-        result = last . take 6 . iterate (enhanceAll rules) $ Map.singleton start 1
+        result = last . take 7 . iterate (enhanceAll rules) $ Map.singleton start 1
+        calculate (ys, count) = length (filter (=='#') ys) * count
 
 enhanceAll :: (Rules, Rules) -> Map.Map String Int -> Map.Map String Int
-enhanceAll rules = foldr (add rules) Map.empty . Map.toList
+enhanceAll rules counts = foldr (add rules) Map.empty $ Map.toList counts
 
 add :: (Rules, Rules) -> (String, Int) -> Map.Map String Int -> Map.Map String Int
 add rules (xs, count) counts = foldr (add' count) counts $ threeSteps rules xs
 
 add' :: Int -> String -> Map.Map String Int -> Map.Map String Int
-add' = undefined
+add' count xs = Map.insertWith (+) xs count
 
 threeSteps :: (Rules, Rules) -> String -> [String]
 threeSteps rules xs = breakup 3 3 step3
     where
-        step1 = enhance rules start -- 3 --> 4
+        step1 = enhance rules xs -- 3 --> 4
         step2 = enhance rules step1 -- 4 --> 6
         step3 = enhance rules step2 -- 6 --> 9
 
 enhance :: (Rules, Rules) -> String -> String
-enhance _ xs | trace (printf "%2d %s" (length xs) xs) False = undefined
 enhance (rules2, _) xs | length xs == 4 = rules2 ! xs
 enhance (_, rules3) xs | length xs == 9 = rules3 ! xs
 enhance rules xs | length xs == 16 = enhance4 rules xs
@@ -64,11 +61,10 @@ enhance4 rules xs = join 2 3 blocks3
         blocks3 = map (enhance rules) blocks2
 
 enhance6 :: (Rules, Rules) -> String -> String
-enhance6 _ xs | trace (show xs) False = undefined
 enhance6 rules xs = join 3 3 blocks3
     where
         blocks2 = breakup 3 2 xs
-        blocks3 = map (enhance rules) $ trace (show blocks2) blocks2
+        blocks3 = map (enhance rules) $ blocks2
 
 enhance9 :: (Rules, Rules) -> String -> String
 enhance9 rules xs = join 3 4 blocks4
